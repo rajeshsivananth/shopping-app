77af9bf7741ebb1ab8e83c3bc1f7731099d32627
Rajesh Kumar <rajeshsivananth@gmail.com>
2022-08-01 18:44:51 +0530

    Change the state onclick of a button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: { first: 'Rajesh', last: 'Siva' },
+      company: 'Stats'
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hey {this.state.name.first} {this.state.name.last}. I work at {this.state.company}</p>
+          <button onClick={() => {
+            this.setState(() => {
+              return {
+                name: {
+                  first: 'Magizh',
+                  last: 'Rajesh'
+                }
+              }
+            }, () => {
+              console.log(this.state);
+            });
+          }}>Change Name</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
